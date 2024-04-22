ac9195420114861561a787b5bd1552963667ce39
Mercenary <oaishazher@gmail.com>
2024-04-23 02:22:56 +0530

    Modify Skeletons

--- src/components/ui/CardGrid.jsx
+++ src/components/ui/CardGrid.jsx
@@ -43,7 +43,6 @@ const CardGrid = ({url}) => {
                                                 ratTop={card.rating_top}
                                             />
                                         ) : null;
-
                                     })
                                 }
                             </> : <Skeleton/>
