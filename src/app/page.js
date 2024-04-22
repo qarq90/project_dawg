84ea27ed0bad4c29cf4acf787d7c4b722bb03f0d
Mercenary <oaishazher@gmail.com>
2024-04-22 17:00:21 +0530

    Done here

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
+import s from "@/styles/pages/global.module.css";
+
 export default function Home() {
     return (
-        <h1>DAWG</h1>
+        <div className={s.container}>
+            <div>
+                <h1 className={s.heading1}>New and trending</h1>
+                <p style={{margin: '8px 4px 0'}}>Based on player counts and release date</p>
+            </div>
+        </div>
     );
 }
