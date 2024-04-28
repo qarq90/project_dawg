2516d2e20b3f2debb83d9a57c14324c624de1737
qarq90 <scythethroughtheheart05@gmail.com>
2024-04-28 18:09:05 +0100

    Game Details Done

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import styledCard from "@/styles/ui/card.module.css"
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import Link from "next/link";
 import {WindowsIcon} from "../../../public/icon/WindowsIcon";
 import {PlaystationIcon} from "../../../public/icon/PlaystationIcon";
@@ -29,6 +29,24 @@ export const Card = (props) => {
     const [fullCard, setFullCard] = useState(false)
 
     const tag = slug
+
+    function gameLikeHandler(e) {
+        e.preventDefault()
+
+        console.log('liked')
+    }
+
+    function gameWishlistHandler(e) {
+        e.preventDefault()
+
+        console.log('wishlist')
+    }
+
+    function gameOwnedHandler(e) {
+        e.preventDefault()
+
+        console.log('owned')
+    }
 
     return (
         <Link
@@ -92,7 +110,7 @@ export const Card = (props) => {
                     <Link href={link ? link : `/game/about/${tag}`}>{gameName}</Link>
                 </div>
                 <div className={styledCard.btnGroup}>
-                    <button>
+                    <button onClick={gameOwnedHandler}>
                         <svg
                             className="SVGInline-svg game-card-button__icon-svg game-card-button__icon_12-svg game-card-button__icon_with-offset-svg"
                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" width="12" height="12">
@@ -103,11 +121,19 @@ export const Card = (props) => {
                         </svg>
                         {likes}
                     </button>
-                    <button style={{opacity: fullCard ? 1 : 0}}>
+                    <button style={{opacity: fullCard ? 1 : 0}} onClick={gameWishlistHandler}>
                         <svg className="SVGInline-svg game-card-button__icon-svg game-card-button__icon_20-svg"
                              xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 30 30">
                             <path fill="#FFF"
                                   d="M25.5 9.846h-4.746a5.87 5.87 0 00.837-.657 3.027 3.027 0 000-4.32c-1.175-1.158-3.223-1.159-4.4 0-.649.639-2.375 3.24-2.137 4.977h-.108c.237-1.738-1.488-4.339-2.138-4.978-1.176-1.158-3.224-1.157-4.4 0a3.028 3.028 0 000 4.321c.205.203.498.429.838.657H4.5A1.487 1.487 0 003 11.314v3.672c0 .405.336.734.75.734h.75v8.812c.004.813.675 1.47 1.5 1.468h18a1.487 1.487 0 001.5-1.468V15.72h.75c.414 0 .75-.329.75-.734v-3.672a1.487 1.487 0 00-1.5-1.468zM9.472 5.904a1.61 1.61 0 011.138-.464c.427 0 .83.164 1.135.464 1.011.995 2.016 3.54 1.667 3.893 0 0-.064.048-.278.048-1.036 0-3.015-1.054-3.662-1.691a1.578 1.578 0 010-2.25zm4.778 18.628H6V15.72h8.25v8.812zm0-10.28H4.5v-2.938h9.75v2.938zm4.005-8.348c.609-.598 1.665-.597 2.273 0a1.578 1.578 0 010 2.25c-.647.637-2.626 1.692-3.662 1.692-.214 0-.278-.047-.279-.049-.348-.354.657-2.898 1.668-3.893zM24 24.532h-8.25V15.72H24v8.812zm1.5-10.28h-9.75v-2.938h9.75v2.938z"></path>
+                        </svg>
+                    </button>
+                    <button style={{opacity: fullCard ? 1 : 0}} onClick={gameLikeHandler}>
+                        <svg className="SVGInline-svg game-card-button__icon-svg game-card-button__icon_20-svg"
+                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="15" height="15">
+                            <path
+                                d="M9.5449219 4C5.9299219 4 3 6.9299219 3 10.544922C3 16.837321 10.298975 22.849799 13.708984 25.527344 A 2 2 0 0 0 13.71875 25.535156C13.742115 25.5535 13.773881 25.579629 13.796875 25.597656L13.798828 25.595703 A 2 2 0 0 0 15 26 A 2 2 0 0 0 16.203125 25.595703L16.203125 25.597656C16.209855 25.59238 16.219801 25.585381 16.226562 25.580078C16.231704 25.576045 16.23898 25.570455 16.244141 25.566406 A 2 2 0 0 0 16.263672 25.548828C19.663109 22.880904 27 16.852336 27 10.544922C27 6.9299219 24.070078 4 20.455078 4C17.000078 4 15 7 15 7C15 7 12.999922 4 9.5449219 4 z"
+                                fill="#FFFFFF"/>
                         </svg>
                     </button>
                 </div>
