36baf046ab70e710392cc4d1b3d4d299195d8191
qarq90 <scythethroughtheheart05@gmail.com>
2024-05-01 23:06:59 +0100

    Project 1.0 - 01/05/2024

--- src/lib/profileObj.js
+++
@@ -1,6 +0,0 @@
-export const profileTabs = [
-    {href: '/pages/profile/profile', label: 'PROFILE'},
-    {href: '/pages/profile/wishlist', label: 'WISHLIST'},
-    {href: '/pages/profile/liked', label: 'LIKED'},
-    {href: '/pages/profile/owned', label: 'OWNED'},
-];
