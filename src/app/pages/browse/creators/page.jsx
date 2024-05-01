ae906d39d146e6d1ad297cff9695baa6462f0623
qarq90 <scythethroughtheheart05@gmail.com>
2024-05-01 23:11:09 +0100

    Project 1.0 - 01/05/2024

--- src/app/pages/browse/creators/page.jsx
+++ src/app/pages/browse/creators/page.jsx
@@ -5,19 +5,6 @@ export default function Creators() {
     return (
         <div className={styledGlobal.container}>
             <h1>Creators</h1>
-            <div className={styledGlobal.dropdownContainer}>
-                <select>
-                    <option value="">Platforms</option>
-                    <option value="">PC</option>
-                    <option value="">Playstation</option>
-                    <option value="">XBox</option>
-                    <option value="">Android</option>
-                    <option value="">IOS</option>
-                    <option value="">MAC</option>
-                    <option value="">Nintendo</option>
-                    <option value="">Linux</option>
-                </select>
-            </div>
             <CardGridX
                 filter={'creator'}
                 url={'https://api.rawg.io/api/creators?key=9560492cd5c24a7cbe8ae7e99bb58971'}
